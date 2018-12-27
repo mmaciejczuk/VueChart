53f7fc22ea92f2f613d59e59c810fc5f5724360d
Mariusz Maciejczuk <mariusz.maciejczuk@intive.com>
2018-12-27 12:08:57 +0100

    delete files

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
     <main-chart></main-chart>
-    <filters></filters>
   </div>
 </template>
 
 <script>
 import MainChart from './components/MainChart'
-import Filters from './components/Filters'
 
 export default {
   name: 'app',
   components: {
-    MainChart,
-    Filters
+    MainChart
   }
 }
 </script>
